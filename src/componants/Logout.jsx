139761f4dc1c4a377502d2c4325791a8b047726d
SamarNegm <negmsama890@gmail.com>
2022-06-04 21:44:03 +0200

    logout

--- src/componants/Logout.jsx
+++ src/componants/Logout.jsx
@@ -7,6 +7,7 @@ import { logoutRoute } from "../utils/APIRoutes";
 export default function Logout() {
   const navigate = useNavigate();
   const handleClick = async () => {
+
     const id = await JSON.parse(
       localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
     )._id;
